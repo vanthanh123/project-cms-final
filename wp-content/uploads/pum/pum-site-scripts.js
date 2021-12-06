4f434ecd316444e4a7c25bb694453e4c300e1f46
tranthuong021001 <tranthuong021001@gmail.com>
2021-12-06 10:08:26 +0700

    delete module 5

--- wp-content/uploads/pum/pum-site-scripts.js
+++ wp-content/uploads/pum/pum-site-scripts.js
@@ -1,6 +1,6 @@
 /**
  * Do not touch this file! This file created by the Popup Maker plugin using PHP
- * Last modified time: Dec 04 2021, 09:11:16
+ * Last modified time: Dec 05 2021, 02:55:54
  */
 
 
