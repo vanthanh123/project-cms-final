9ef86e2c985345fa05185e551ef911f36a68d806
vanthanh <thanhssntc2@gmail.com>
2021-12-08 17:25:04 +0700

    complete mini cart v1

--- wp-content/plugins/woocommerce/includes/widgets/class-wc-widget-cart.php
+++ wp-content/plugins/woocommerce/includes/widgets/class-wc-widget-cart.php
@@ -19,7 +19,7 @@ class WC_Widget_Cart extends WC_Widget {
 	 * Constructor.
 	 */
 	public function __construct() {
-		$this->widget_cssclass    = 'woocommerce widget_shopping_cart';
+		$this->widget_cssclass    = 'custom_mini_cart woocommerce widget_shopping_cart';
 		$this->widget_description = __( 'Display the customer shopping cart.', 'woocommerce' );
 		$this->widget_id          = 'woocommerce_widget_cart';
 		$this->widget_name        = __( 'Cart', 'woocommerce' );
